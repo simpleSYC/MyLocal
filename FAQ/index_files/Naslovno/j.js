bc040f9c91ae7c7993f72638b295b9cd57a57737
simple <em@il.com>
2021-04-18 02:19:19 +0100

    Update j.js

--- FAQ/index_files/Naslovno/j.js
+++ FAQ/index_files/Naslovno/j.js
@@ -1,6 +1,8 @@
 Vue.component("naslovo", {
     template: `<div> 
 <div id="google_translate_element"></div>
+<script type="text/javascript">function googleTranslateElementInit() {
+  new google.translate.TranslateElement({pageLanguage: 'en', layout: google.translate.TranslateElement.InlineLayout.HORIZONTAL}, 'google_translate_element');}</script>  
 <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
 </div>`})
 var store="";
@@ -14,6 +16,3 @@ var store="";
     el:"#Naslovnoto",
     store: store
     })
-
-function googleTranslateElementInit() {
-new google.translate.TranslateElement({pageLanguage: 'en', layout: google.translate.TranslateElement.InlineLayout.HORIZONTAL}, 'google_translate_element');}
