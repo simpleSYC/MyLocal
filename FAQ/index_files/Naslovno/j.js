4b5e7ceb88c83490e2598279a67de2a089902b4e
simple <em@il.com>
2021-04-18 01:59:33 +0100

    .

--- FAQ/index_files/Naslovno/j.js
+++ FAQ/index_files/Naslovno/j.js
@@ -1,13 +1,13 @@
 Vue.component("naslovo", {
     template: `<div> 
-<p style="margin-left: 10%;margin-top: 20px;" id="google_translate_element">
+<a style="margin-left: 10%;margin-top: 20px;" id="google_translate_element">
 <div class="skiptranslate goog-te-gadget" style="display: none;" dir="ltr">
 <div id=":0.targetLanguage" style="display: inline;">
 <select class="goog-te-combo" aria-label="Language Translate Widget">
 </select></div>&nbsp;&nbsp;Powered by <span style="white-space:nowrap">
 <a class="goog-logo-link" href="https://translate.google.com" target="_blank">
 <img width="37" height="14" style="padding-right: 3px" alt="Google Translate" src="https://www.gstatic.com/images/branding/googlelogo/1x/googlelogo_color_42x16dp.png">Translate</a>
-</span></div></p>
+</span></div></a>
 
 </div>`})
 var store="";
@@ -24,7 +24,3 @@ var store="";
 
 function googleTranslateElementInit() {
 new google.translate.TranslateElement({pageLanguage: 'en', layout: google.translate.TranslateElement.InlineLayout.HORIZONTAL}, 'google_translate_element');}
-      
-
-
-   
