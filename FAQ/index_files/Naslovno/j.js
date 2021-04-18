447f6d9c2075431e32a7cd141b18767846f8380e
simple <em@il.com>
2021-04-18 01:28:27 +0100

    .

--- FAQ/index_files/Naslovno/j.js
+++ FAQ/index_files/Naslovno/j.js
@@ -10,10 +10,12 @@ Vue.component("naslovo", {
 <img width="37" height="14" style="padding-right: 3px" alt="Google Translate" src="https://www.gstatic.com/images/branding/googlelogo/1x/googlelogo_color_42x16dp.png">Translate</a>
 </span></div></a>
 
+
 <script type="text/javascript">
 function googleTranslateElementInit() {
 new google.translate.TranslateElement({pageLanguage: 'en', layout: google.translate.TranslateElement.InlineLayout.HORIZONTAL}, 'google_translate_element');}</script>
-   
+<script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+
 
 </div>`})
 var store="";
