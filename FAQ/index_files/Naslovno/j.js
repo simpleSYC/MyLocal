d3a644ce5982737b4a5740824e7c3c3b266532bb
simple <em@il.com>
2021-04-18 01:14:52 +0100

    b

--- FAQ/index_files/Naslovno/j.js
+++ FAQ/index_files/Naslovno/j.js
@@ -1,6 +1,6 @@
-Vue.component("headersko", {
+Vue.component("headot", {
     template: `<div>
-    
+   
 <a style="margin-left: 10%;margin-top: 20px;" id="google_translate_element">
 <div class="skiptranslate goog-te-gadget" style="display: none;" dir="ltr">
 <div id=":0.targetLanguage" style="display: inline;">
@@ -14,19 +14,21 @@ Vue.component("headersko", {
 function googleTranslateElementInit() {
 new google.translate.TranslateElement({pageLanguage: 'en', layout: google.translate.TranslateElement.InlineLayout.HORIZONTAL}, 'google_translate_element');}</script>
 <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
-
-
+   
 
 
 </div>`})
 var store="";
     var storeqqee = new Vuex.Store({
     state: {
-      view:"headersko"
+      view:"headot"
     }
     })   
 
     var signqqee = new Vue({
-    el:"#Headersko",
+    el:"#Head",
     store: store
     })
+
+
+   
